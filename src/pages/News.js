9dbe8fa9394de7b28f8ec310e66c46c6fe3d9e20
tachung <tachung@naver.com>
2024-02-15 12:29:04 +0900

    Update News.js

--- src/pages/News.js
+++ src/pages/News.js
@@ -14,7 +14,7 @@ function News() {
 
     useEffect(() => {
       // GET 요청을 보낼 API 주소
-      const apiUrl = 'https://port-0-belleravihomeapi-1gksli2alpurg9zr.sel4.cloudtype.app/api/newslist';
+      const apiUrl = 'https:/belleravi.co.kr/api/newslist';
 
       // fetch를 사용하여 GET 요청 보내기
       fetch(apiUrl)
