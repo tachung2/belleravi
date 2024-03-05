5a76e350b0fb677e0cb9ba92f02c9565bc561551
tachung <tachung@naver.com>
2024-03-05 16:38:27 +0900

    Update Product.js

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -8,7 +8,7 @@ function Product() {
 
     useEffect(() => {
         // GET 요청을 보낼 API 주소
-        const apiUrl = 'https://belleravi.co.kr/api/productlist';
+        const apiUrl = 'http://belleravi.co.kr/api/productlist';
 
         // fetch를 사용하여 GET 요청 보내기
         fetch(apiUrl)
